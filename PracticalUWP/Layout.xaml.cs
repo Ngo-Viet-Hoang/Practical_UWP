9cc3b117f7fb876d3c2732e522536f302739af50
ADMIN <ADMIN@DESKTOP-U76SBMK>
2021-11-09 12:31:51 +0700

    Add project files.

---
+++ PracticalUWP/Layout.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace PracticalUWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Layout : Page
+    {
+        public Layout()
+        {
+            this.InitializeComponent();
+        }
+
+        private void PracticalUWP_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            PracticalUWP.IsBackEnabled = true;
+            PracticalUWP.IsBackButtonVisible = NavigationViewBackButtonVisible.Visible;
+            if (args.IsSettingsInvoked)
+            {
+                ContentPage.Navigate(typeof(Pages.SettingPage), args.RecommendedNavigationTransitionInfo);
+                return;
+            }
+
+            var tag = args.InvokedItemContainer.Tag.ToString();
+            switch (tag)
+            {
+                case "addcontact":
+                    ContentPage.Navigate(typeof(Pages.AddContact), args.RecommendedNavigationTransitionInfo);
+                    break;
+                case "searchcontact":
+                    ContentPage.Navigate(typeof(Pages.SearchContact), args.RecommendedNavigationTransitionInfo);
+                    break;
+
+            }
+
+        }
+
+        private void PracticalUWP_BackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
+        {
+            if (ContentPage.CanGoBack)
+            {
+                ContentPage.GoBack();
+            }
+
+        }
+    }
+}
